7305e793f415eb96746424cc401a9f74b471315c
nikitafloy <alkoskal@gmail.com>
2020-08-14 19:06:28 +0500

    refactor the project

--- admins.ts
+++ admins.ts
@@ -1,25 +1,23 @@
-import api from 'node-vk-bot-api/lib/api';
+import * as api from 'node-vk-bot-api/lib/api';
 import keys from './keys/index';
 import {Iparams} from './Interfaces/bot/Iparams';
 
-const getAdmins = (): Promise<String | Boolean> => {
-  const params: Iparams = {
-    group_id: keys.GROUP_ID,
-    access_token: keys.TOKEN,
-    fields: 'contacts',
-  };
-
-  return api('groups.getById', params)
-    .catch(e => console.error(e))
-    .then(res => {
-      if (res.response && typeof res.response === 'object') {
-        let admins: Array<number> = [];
-        res.response[0].contacts.forEach(item => admins.push(item.user_id));
-        return admins.join(',');
-      } else {
-        return false;
-      };
-    });
+const params: Iparams = {
+  group_id: keys.GROUP_ID,
+  access_token: keys.TOKEN,
+  fields: 'contacts',
 };
 
-module.exports = getAdmins;
+export default async (): Promise<String | Boolean> => {
+  try {
+    const contacts = await api('groups.getById', params);
+    if (contacts.response && typeof contacts.response === 'object') {
+      let admins: Array<number> = [];
+      contacts.response[0].contacts.forEach(item => admins.push(item.user_id));
+      return admins.join(',');
+    };
+  } catch (e) {
+    console.error(e);
+  };
+  return false;
+};
--- index.ts
+++ index.ts
@@ -9,6 +9,9 @@ import {Ictx} from './Interfaces/bot/Ictx';
 import {Iparams} from './Interfaces/bot/Iparams';
 import {Ibot} from './Interfaces/bot/Ibot';
 
+// Get VK Group Admins
+import getAdmins from './admins';
+
 // Markup
 import * as Markup from 'node-vk-bot-api/lib/markup';
 
@@ -34,8 +37,7 @@ const startKeyBoard = (ctx: Ictx): void => {
 };
 
 const showCategories = (ctx: Ictx): void => ctx.reply(
-  messages.ON_START, 
-  null,
+  messages.ON_START, null,
   Markup
     .keyboard(messages.CATEGORIES, { columns: 2 })
     .inline()
@@ -56,73 +58,78 @@ bot.on(async (ctx: Ictx): Promise<any> => {
     ctx.session.step = 1;
 
     // Не введено имя, человек обратился впервые
-    if (!ctx.session.name) {
-      const params: Iparams = {
-        user_ids: user_id,
-        fields: 'first_name',
-        access_token: keys.TOKEN,
-      };
-    
-      return await api('users.get', params)
-        .catch(e => {
-          console.error(e);
-          ctx.reply(messages.FAIL);
-        })
-        .then(res => {
-          const name: String = res.response[0].first_name;
+    if (!ctx.session.name) {    
+      try {
+        const params: Iparams = {
+          user_ids: user_id,
+          fields: 'first_name',
+          access_token: keys.TOKEN,
+        };  
+
+        const user = await api('users.get', params);
+        if (user) {
+          const name: String = user.response[0].first_name;
           ctx.reply(messages.GET_PHONE(name));
           ctx.session.target = message;
           ctx.session.name = name;
           ctx.session.step = 2;
-        }); 
+        } else {
+          console.error(messages.CANT_GET_NAME(user_id));
+          ctx.reply(messages.FAIL);  
+        };
+      } catch (e) {
+        console.error(e);
+        ctx.reply(messages.FAIL);      
+      };
     } else {
       // Есть имя, человек обращается повторно, просим телефон
+      ctx.session.target = message;
       return ctx.reply(messages.GET_PHONE(ctx.session.name));
     };
     showCategories(ctx);
   } else {
     // Какой-то текст, не из категорий
-
-    // Нужен этап, первый, когда мы запрашиваем номер и в конце, даем возможность вернуться в начало
     if (!ctx.session.step) {
       return showCategories(ctx);
     };
 
-    // Актуальный номер телефона или нет
     // Введен номер телефона
     if (message.match(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/)) {
+
       // Благодарим за номер, закрыли сессию
       ctx.session.phone = message;
+      ctx.session.url = `https://vk.com/id${user_id}`;
       closeSession(ctx, messages.WE_WILL_CALL);
 
-      ctx.session.url = `https://vk.com/id${user_id}`;
-
-      const {url, name, phone, target} = ctx.session;
-
       // Get Admins
-      require('./admins')()
-        .catch(e => console.error(e))
-        .then(admins => {
-          if (!admins) {
-            return console.log(messages.CANT_GET_ADMINS(phone, url));
-          };
-          
+      try {
+        const admins = await getAdmins();
+        const {url, name, phone, target} = ctx.session;
+
+        if (admins) {
           // Choose the method depending on the number of people
-          const ids_method = admins.match(/,/) ? 'user_ids' : 'user_id';
+          const ids_method = typeof admins === 'string' && admins.match(/,/) ? 'user_ids' : 'user_id';
           const params: Iparams = {
             [ids_method]: admins,
             random_id: Math.ceil(Math.random() * 1000 + 1),
             message: messages.MSG_TO_MANAGER(url, name, phone, target),
             access_token: keys.TOKEN,
           };
-
-          api('messages.send', params)
-            .catch(e => {
-              console.error(e);
-              ctx.reply(messages.FAIL);
-            })
-            .then(res => console.log(messages.CONSOLE_END));
-        });
+    
+          try {
+            if (await api('messages.send', params)){
+              console.log(messages.CONSOLE_END);
+            };
+          } catch (e) {
+            console.error(e);
+            ctx.reply(messages.FAIL);
+          };
+        } else {
+          console.log(messages.CANT_GET_ADMINS(phone, url))
+        };
+      } catch (e) {
+        console.error(e);
+      };
     } else {
       return ctx.reply(messages.INCORRECT_PHONE);
     };  
